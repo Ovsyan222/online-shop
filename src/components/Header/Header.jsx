33958044bf732d373906732896fa0b0431f99e99
Ovsyan222 <ovsyansuper228@gmail.com>
2025-12-09 17:20:09 +0300

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <nav className='navbar'>
+                <img src="public\Logo.png" alt="logo" className='logo'/>
+                <div className='menu'>
+                    <ul className='menu-item'>
+                        <li className='item'>Каталог</li>
+                        <li className='item'>Обо мне</li>
+                        <li className='item'>Обратная связь</li>
+                    </ul>
+                </div>
+                <button className='basketButton'>Корзина<img src="public\iconBasket.png" alt="basket" /></button>
+                <button className='account'>Личный кабинет<img src="public\iconAccount.png" alt="account" /></button>
+            </nav>
+        </header>
+    )
+}
